2d5c7cff356b17aa929a0dea76068b8be73b3992
Edgardo Mendoza <edgardoamh01@gmail.com>
2022-09-02 20:06:14 -0400

    Se añaden los archivos principales del proyecto
    Se agregan los archivos package.json, gulpfile.js, los respectivos archivos de SCSS y otros en general

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+sass.compiler = require('dart-sass');
+
+function compilarSASS() {
+    return src("./src/scss/app.scss")
+        .pipe( sass() )
+        .pipe( dest("./build/css") );
+}
+
+
+exports.compilarSASS = compilarSASS
